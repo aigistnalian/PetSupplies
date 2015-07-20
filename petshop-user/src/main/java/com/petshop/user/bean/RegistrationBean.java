155e85219378ad0bfa0df5506564f8777cf782e4
aigistnalian <aigistnalian@gmail.com>
2015-07-20 17:44:04 +0530

    Added for registration

--- petshop-user/src/main/java/com/petshop/user/bean/RegistrationBean.java
+++ petshop-user/src/main/java/com/petshop/user/bean/RegistrationBean.java
@@ -5,7 +5,11 @@ package com.petshop.user.bean;
 
 import java.io.Serializable;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.TabChangeEvent;
 
 /**
  * @author analian
@@ -14,13 +18,35 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name="registrationBean")
 public class RegistrationBean implements Serializable
 {
+	 private RegistrationForm registrationForm = null;
 
 	/**
 	 * Default value
 	 */
 	private static final long serialVersionUID = 1L;
 	
-	
+	public void onTabChange(TabChangeEvent event) {
+		System.out.println(event);
+		 FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
+	        FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	/**
+	 * @return the registrationForm
+	 */
+	public RegistrationForm getRegistrationForm() {
+		 if(this.registrationForm == null){
+			   this.registrationForm = new RegistrationForm();
+			  }
+		return registrationForm;
+	}
+
+	/**
+	 * @param registrationForm the registrationForm to set
+	 */
+	public void setRegistrationForm(RegistrationForm registrationForm) {
+		this.registrationForm = registrationForm;
+	}
 
 	
 }
